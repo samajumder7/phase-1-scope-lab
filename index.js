76404c960c48cd045044dfdcf3c2656aa6608313
Saswata Alex Majumder <majumder.ua@gmail.com>
2023-09-25 13:42:47 -0400

    scope lab done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+// EAXMPLES OF BAD CODING PRACTICES.
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = 'sue';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'pat';
+}
